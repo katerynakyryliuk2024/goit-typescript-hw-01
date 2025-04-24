f355f550cc29883d8dbb9c515f6465832bf0aba5
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-04-24 13:56:06 +0300

    create 1.ts-4.ts

---
+++ src/basic/1.ts
@@ -0,0 +1,17 @@
+const age: number = 50;
+console.log(age);
+
+const username: string = "Max";
+console.log(username);
+
+const toggle: boolean = true;
+console.log(toggle);
+
+const empty: null = null;
+console.log(empty);
+
+const callback = (a: number) => {
+  return 100 + a;
+};
+
+callback(10);
---
+++ src/basic/3.ts
@@ -0,0 +1,11 @@
+let mixedType: string | number;
+
+mixedType = 10;
+
+type OneOrTwo = "enable" | "disable";
+
+let value: OneOrTwo;
+
+value = "enable";
+
+value = "disable";
